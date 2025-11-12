90747b721843181983cb7bdf58d0592908a60f7e
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2025-11-12 09:16:54 +0530

    refactor: optimization

--- voice_transcipter.py
+++ voice_transcipter.py
@@ -1,64 +1,19 @@
-import os
-import wave
-import pyaudio
-import librosa
-import whisper
 import threading
 import numpy as np
 from faster_whisper import WhisperModel
+from scipy.signal import resample_poly # replace librosa for better speed but quality still good
 import warnings
 warnings.filterwarnings("ignore")
 
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNKS = 1024 # frames_per_buffer
-p = pyaudio.PyAudio()
+class VoiceTranscriptor:
+    def __init__(self, model_name:str = "tiny.en"):
+        self._model = WhisperModel(model_name)
 
-recording = False
-model = whisper.load_model("small.en")  # use tiny.en, small.en, or base.en for English-only
-frames = []
-def record():
-    global frames
-    stream = p.open(
-        format = FORMAT,
-        channels = CHANNELS,
-        rate = RATE,
-        frames_per_buffer = CHUNKS,
-        input = True
-    )
-    # frames = []
-    frames.clear()
-    while recording:
-        try:
-            data = stream.read(
-            CHUNKS,
-            exception_on_overflow = False)
-            
-            data = np.frombuffer(data, dtype=np.int16).astype(np.float32)/32768.0
-            data = librosa.resample(data, orig_sr=44100, target_sr=16000)
-            frames.append(data)
-        except Exception as e:
-            print(e)
-            break
-    stream.stop_stream()
-    stream.close()
-
-if __name__ == "__main__":
-    input("Enter to start the recording: ")
-    recording = True
-    task = threading.Thread(target = record, daemon = True)
-    task.start()
-    try:
-        input()
-    except KeyboardInterrupt:
-        print("exception")
-    finally:
-        recording =False
-        task.join()
-        p.terminate()
-        print(len(frames))
-        if frames:
-            frame = np.concatenate(frames)
-            result = model.transcribe(frame, language='english')
-            print(result.get("text"))
+    def transcribe(self, audio_bytes):
+        audio_bytes = b"".join(audio_bytes)
+        audio_bytes = np.frombuffer(audio_bytes, dtype = np.int16).astype(np.float32)/32768.0
+        audio_bytes = resample_poly(audio_bytes, up=16000, down = 44100)
+        segments, info = self._model.transcribe(audio_bytes)
+        transcription = " ".join([segment.text for segment in segments])
+        print(transcription)
+        return transcription
